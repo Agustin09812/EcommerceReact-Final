5448e1f5a1a1949fa0d08e3047e9541eddf4f641
Agustin09812 <agustinivanlago@gmail.com>
2023-11-17 00:03:59 -0300

    third push

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Aumentamos el chunkSizeWarningLimit para que deployee bien
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+  
   plugins: [react()],
   
   // agregamos el BASE para el gh-pages
